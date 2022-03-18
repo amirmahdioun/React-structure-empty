542039d9a78c2ac8faaaa87ab35322e3c07756ac
amir.mahdioun <amir.mahdioun@yahoo.com>
2022-03-18 23:47:27 +0330

    feat : App route added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
+import {AppRoute} from "./route/App.route";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AppRoute />
   </React.StrictMode>,
   document.getElementById('root')
 );
