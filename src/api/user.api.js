1bcb45c4e4b2b3f64051eff9db9dd770142e9b1e
amir.mahdioun <amir.mahdioun@yahoo.com>
2022-05-12 11:07:54 +0430

    feat : api functions feature added to api folder

---
+++ src/api/user.api.js
@@ -0,0 +1,11 @@
+//In this file you can write your api function to get/post/put/patch an api and call it everywhere you want
+//All api functions should be here in api folder
+
+
+const userApi = async() => {
+    try{
+
+    }catch (e) {
+
+    }
+}
